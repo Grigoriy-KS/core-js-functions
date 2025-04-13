8a36cf43870028d2cfbfca93f12e70a8d7f7dcd0
Grigoriy Ksyonz <KsyonzGS@gmail.com>
2025-04-13 23:55:17 +0300

    feat(src): add solution to task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -237,8 +237,13 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let currentId = startFrom;
+  return function idGeneratorFunction() {
+    const idToReturn = currentId;
+    currentId += 1;
+    return idToReturn;
+  };
 }
 
 module.exports = {
